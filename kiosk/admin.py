d1fdf721bd8d5e67aa63ca8e9c659d069e742ef1
iseunglee <iseunglee99@gmail.com>
2024-04-04 21:38:49 +0900

    4월4일 수업내용

--- kiosk/admin.py
+++ kiosk/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from kiosk.models import DrinkType
 
-# Register your models here.
+@admin.register(DrinkType)
+class DrinkTypeAdmin(admin.ModelAdmin):
+    list_play = ('type_name', 'add_date')
